34289883a46e048c1fb2009c7f2438fafd3f8ee6
CreativeX42 <105191590+CreativeX42@users.noreply.github.com>
2022-05-11 12:12:08 +0300

    Initial_Commit

---
+++ Summator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Summator // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = Summator.Sum(new int[] { 1, 2, 3, 4, 5, 6, });
+            if (result == 21)
+            {
+                Console.WriteLine("Test Pass");
+            }
+            else
+            {
+                Console.WriteLine("Test fail");
+            }
+        }
+    }
+}
